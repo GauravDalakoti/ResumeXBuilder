34c33648c89c013795629caa0425935493e7df48
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-08-14 13:20:48 +0530

    change cors url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: [""],
+        origin: ["https://resume-x-builder-frontend.vercel.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
