a4be4cc8e10896371cd9d4679d4dab72c754ef3b
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-08-14 13:06:18 +0530

    change the cors origin

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(cors(
     {
-        origin: ["https://resume-builder-x.vercel.app"],
+        origin: [""],
         methods: ["POST", "GET"],
         credentials: true
     }
