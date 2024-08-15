e5d758a9aa0d73822ae13c355d941a8ab9199760
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-08-15 14:36:10 +0530

    hello one

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -31,8 +31,7 @@ function Login() {
 
           'Content-Type': "application/json"
         },
-        body: JSON.stringify(user),
-        withCredentials: true
+        body: JSON.stringify(user)
 
       });
 
