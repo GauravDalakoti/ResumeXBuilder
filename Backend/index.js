91f5b22cdd5744740b99da782fcbae81d67755ee
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-08-15 10:56:24 +0530

    changes in vercel

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,11 @@
 // require('dotenv').config({path:"./env"})
 
 import dotenv from "dotenv"
-import connectDB from "./db/index.js"
-import app from "./app.js"
+// import connectDB from "./db/index.js"
+// import app from "./app.js"
+
+import app from "./src/app.js"
+import connectDB from "./src/db/index.js"
 
 dotenv.config({ path: "./.env" })
 
