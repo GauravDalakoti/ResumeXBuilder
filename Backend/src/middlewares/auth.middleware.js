65ac7230fd6f5b3c486841ef12e9e0b734dd2889
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-08-15 12:37:29 +0530

    change routers

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -17,7 +17,6 @@ export const verifyJWT = asyncHandler(async (req, res, next) => {
         const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
 
         const user = await User.findById(decodedToken?._id).select("-password -refreshToken")
-        console.log(user);
 
         if (!user) {
 
