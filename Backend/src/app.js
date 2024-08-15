339895599153936132badb3665ce43bf67b0d44e
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-08-15 13:44:23 +0530

    add proxy

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -13,6 +13,7 @@ app.use(cors(
     }
 ))
 
+app.set("trust proxy", 1)
 app.use(express.json({ limit: "16kb" }))
 app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 app.use(express.static("public"))
