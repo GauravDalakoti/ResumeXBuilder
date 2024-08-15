aae0112a03a2c5b8ec6631528584affa10cda2a9
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-08-15 12:57:28 +0530

    add credentials

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -20,7 +20,8 @@ function Header() {
 
                 'Content-Type': "application/json"
             },
-            body: JSON.stringify()
+            // body: JSON.stringify()
+            credentials:"include"
         })
         const res = response.json()
 
