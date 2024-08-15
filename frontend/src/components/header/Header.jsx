475791a654773e9de9fd9535f4f49e786e225cf5
gaurav dalakoti <gauravdalakoti046@gmail.com>
2024-08-15 12:44:07 +0530

    change header

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -16,6 +16,11 @@ function Header() {
         const response = await fetch("https://resume-builder-x.vercel.app/api/v1/users/logout", {
 
             method: "POST",
+            headers: {
+
+                'Content-Type': "application/json"
+            },
+            body: JSON.stringify()
         })
         const res = response.json()
 
@@ -29,7 +34,7 @@ function Header() {
             setUserData({ username: "", email: "" })
 
         }
-        console.log(res);
+        console.log(response);
     }
 
     const [displayMenu, setDisplayMenu] = useState(false)
